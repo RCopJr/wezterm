6605470328b9d276ac7e3da4a3585e91c4fb1227
Ramonito Copreros Jr <ramonito@ontariovirtualschool.ca>
2025-07-09 13:56:44 -0400

    small UI changes

--- wezterm.lua
+++ wezterm.lua
@@ -7,10 +7,15 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 28
 
-config.font_size = 16
-config.color_scheme = "rose-pine"
+config.font_size = 19
 
-config.window_background_opacity = 0.95
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.85
+
+config.macos_window_background_blur = 10
 
 config.font = wezterm.font_with_fallback({ "JetBrains Mono" })
 
