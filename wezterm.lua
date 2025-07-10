a29ae562a8862ceb4701b24468e24d788188f556
Ramonito Copreros Jr <ramonito@ontariovirtualschool.ca>
2025-07-10 16:43:08 -0400

    Update font size and styling

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,8 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 28
 
-config.font_size = 19
+config.font_size = 24
+config.line_height = 1.05
 
 config.enable_tab_bar = false
 
@@ -17,7 +18,7 @@ config.window_background_opacity = 0.90
 
 config.macos_window_background_blur = 20
 
-config.font = wezterm.font_with_fallback({ "JetBrains Mono" })
+config.font = wezterm.font_with_fallback({ "JetBrains Mono" }, { weight = "Bold" })
 
 -- Finally, return the configuration to wezterm:
 return config
