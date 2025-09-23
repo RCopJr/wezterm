f7f6886ed837edde7592d7d4ec0157bea42ef820
Ramonito Copreros Jr <ramonito@ontariovirtualschool.ca>
2025-09-23 16:21:25 -0400

    Initial lackluster-setup

--- wezterm.lua
+++ wezterm.lua
@@ -8,82 +8,58 @@ config.initial_cols = 120
 config.initial_rows = 28
 
 config.font_size = 22
-config.line_height = 1.05
+config.line_height = 1
 
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
 config.colors = {
-	foreground = "#D8DEE9", -- main text
-	background = "#1A1E26", -- almost black Nordic background
+	foreground = "#cccccc",
+	background = "#141414", -- darker background
 
-	cursor_bg = "#D8DEE9",
-	cursor_border = "#D8DEE9",
-	cursor_fg = "#2E3440",
+	cursor_bg = "#cccccc",
+	cursor_border = "#cccccc",
+	cursor_fg = "#1a1a1a",
 
-	selection_bg = "#3E4452",
-	selection_fg = "#D8DEE9",
-
-	scrollbar_thumb = "#4C566A",
-	split = "#3B4252",
+	selection_bg = "#444444",
+	selection_fg = "#ffffff",
 
 	ansi = {
-		"#3B4252", -- black
-		"#BF616A", -- red
-		"#A3BE8C", -- green
-		"#EBCB8B", -- yellow
-		"#81A1C1", -- blue
-		"#B48EAD", -- magenta
-		"#88C0D0", -- cyan
-		"#E5E9F0", -- white
+		"#1a1a1a", -- black
+		"#cc6666", -- red
+		"#b5bd68", -- green
+		"#f0c674", -- yellow
+		"#81a2be", -- blue
+		"#b294bb", -- magenta
+		"#8abeb7", -- cyan
+		"#cccccc", -- white
 	},
 
 	brights = {
-		"#4C566A", -- bright black
-		"#BF616A", -- bright red
-		"#A3BE8C", -- bright green
-		"#EBCB8B", -- bright yellow
-		"#81A1C1", -- bright blue
-		"#B48EAD", -- bright magenta
-		"#8FBCBB", -- bright cyan
-		"#ECEFF4", -- bright white
-	},
-
-	tab_bar = {
-		background = "#2E3440",
-		active_tab = {
-			bg_color = "#81A1C1",
-			fg_color = "#2E3440",
-		},
-		inactive_tab = {
-			bg_color = "#3B4252",
-			fg_color = "#D8DEE9",
-		},
-		inactive_tab_hover = {
-			bg_color = "#434C5E",
-			fg_color = "#ECEFF4",
-		},
-		new_tab = {
-			bg_color = "#2E3440",
-			fg_color = "#D8DEE9",
-		},
-		new_tab_hover = {
-			bg_color = "#81A1C1",
-			fg_color = "#2E3440",
-		},
+		"#555555", -- bright black (gray)
+		"#ff3333", -- bright red
+		"#ddee77", -- bright green
+		"#ffd866", -- bright yellow
+		"#6fb3d2", -- bright blue
+		"#c397d8", -- bright magenta
+		"#66cccc", -- bright cyan
+		"#ffffff", -- bright white
 	},
 }
 
-config.window_background_opacity = 0.92
+config.window_background_opacity = 0.88
 
 config.macos_window_background_blur = 10
 
 config.font = wezterm.font("FiraCode Nerd Font", { weight = "Medium" })
+-- config.font = wezterm.font("JetBrainsMono Nerd Font")
+-- config.font = wezterm.font("GohuFont 14 Nerd Font")
+-- config.font = wezterm.font("BigBlueTerm437 Nerd Font")
 
 config.window_padding = {
-	left = 0,
-	right = 0,
+	left = 10,
+	right = 10,
 	top = 15,
 	bottom = 0,
 }
