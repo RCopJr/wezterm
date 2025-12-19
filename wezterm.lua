4d235cf62c164ec3dce814c93de8b0599a73d9df
Ramonito Copreros Jr <ramonito@ontariovirtualschool.ca>
2025-12-19 15:48:49 -0500

    Update again

--- wezterm.lua
+++ wezterm.lua
@@ -14,53 +14,65 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
+-- config.color_scheme = "Oxocarbon Dark (Gogh)"
+-- config.color_scheme = 'rose-pine'
+config.color_scheme = "rose-pine"
 config.colors = {
-	foreground = "#cccccc",
-	background = "#141414", -- darker background
-
-	cursor_bg = "#cccccc",
-	cursor_border = "#cccccc",
-	cursor_fg = "#1a1a1a",
-
-	selection_bg = "#444444",
-	selection_fg = "#ffffff",
-
-	ansi = {
-		"#1a1a1a", -- black
-		"#cc6666", -- red
-		"#b5bd68", -- green
-		"#f0c674", -- yellow
-		"#81a2be", -- blue
-		"#b294bb", -- magenta
-		"#8abeb7", -- cyan
-		"#cccccc", -- white
-	},
-
-	brights = {
-		"#555555", -- bright black (gray)
-		"#ff3333", -- bright red
-		"#ddee77", -- bright green
-		"#ffd866", -- bright yellow
-		"#6fb3d2", -- bright blue
-		"#c397d8", -- bright magenta
-		"#66cccc", -- bright cyan
-		"#ffffff", -- bright white
-	},
+	background = "#0a0a0a",
 }
 
-config.window_background_opacity = 0.88
+-- config.colors = {
+-- 	foreground = "#cccccc",
+-- 	background = "#141414", -- darker background
+--
+-- 	cursor_bg = "#cccccc",
+-- 	cursor_border = "#cccccc",
+-- 	cursor_fg = "#1a1a1a",
+--
+-- 	selection_bg = "#444444",
+-- 	selection_fg = "#ffffff",
+--
+-- 	ansi = {
+-- 		"#1a1a1a", -- black
+-- 		"#cc6666", -- red
+-- 		"#b5bd68", -- green
+-- 		"#f0c674", -- yellow
+-- 		"#81a2be", -- blue
+-- 		"#b294bb", -- magenta
+-- 		"#8abeb7", -- cyan
+-- 		"#cccccc", -- white
+-- 	},
+--
+-- 	brights = {
+-- 		"#555555", -- bright black (gray)
+-- 		"#ff3333", -- bright red
+-- 		"#ddee77", -- bright green
+-- 		"#ffd866", -- bright yellow
+-- 		"#6fb3d2", -- bright blue
+-- 		"#c397d8", -- bright magenta
+-- 		"#66cccc", -- bright cyan
+-- 		"#ffffff", -- bright white
+-- 	},
+-- }
 
-config.macos_window_background_blur = 10
+config.window_background_opacity = 0.92
 
-config.font = wezterm.font("FiraCode Nerd Font", { weight = "Medium" })
--- config.font = wezterm.font("JetBrainsMono Nerd Font")
--- config.font = wezterm.font("GohuFont 14 Nerd Font")
+config.macos_window_background_blur = 0
+
+-- config.font = wezterm.font("FiraCode Nerd Font")
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
+-- config.font = wezterm.font("Hack Nerd Font Mono")
+-- config.font = wezterm.font("UbuntuMono Nerd Font")
+-- config.font = wezterm.font("MesloLGM Nerd Font")
+-- config.font = wezterm.font("BlexMono Nerd Font", { italic = false })
+-- config.font = wezterm.font("GohuFont 14 Nerd Font",  { weight = "Bold" })
 -- config.font = wezterm.font("BigBlueTerm437 Nerd Font")
+-- config.font = wezterm.font("DepartureMono Nerd Font")
 
 config.window_padding = {
 	left = 10,
 	right = 10,
-	top = 15,
+	top = 10,
 	bottom = 0,
 }
 
